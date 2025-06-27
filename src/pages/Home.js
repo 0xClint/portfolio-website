8145cab6a50fc837a2f37343e74440b9f2154877
Omkar <97947439+0xClint@users.noreply.github.com>
2025-06-27 11:59:47 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,18 @@ const Home = () => {
       <div className="projectItem">
         <Projects />
       </div>
-
+      <iframe
+        src="https://supportly-payment-mediater.onrender.com/chat.html?userId=sptly-94033f21c2de4dafz&projectId=cb2d5bcb-4034-4a1a-9ee4-1726ff8bbf3c&dataUrl=https%3A%2F%2Fgateway.pinata.cloud%2Fipfs%2Fbafkreiecondma6bori6idixh22cnnbq6y4hjt3xxz7fues3fqdpfhkwdo4"
+        width="300"
+        height="400"
+        style={{
+          position: "fixed",
+          bottom: "20px",
+          right: "20px",
+          border: "none",
+          zIndex: 9999,
+        }}
+      ></iframe>
       <Contact />
       {/* ***************************************************************************** */}
 
